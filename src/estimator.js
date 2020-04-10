02fea27384cb0af5eedd487352596a4817ff068a
Ebere <ebereuzodufa@gmail.com>
2020-04-11 00:32:22 +0100

    Challenge 1 - Make reportedCases and infectionsByRequestedTime for impact and severImpact
    Ensuring that the periods where adequately converted to days and added appropriately to impact and severImpact.

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,42 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const fatObj = {};
+  const dayPeriodFn = (periodType, periodQty) => {
+    // Infection doubles every three days
+    // I used the modulus to be ensure it is exactly 3 days
+    // eslint-disable-next-line no-nested-ternary
+    const days = (periodType === 'days') ? periodQty : (periodType === 'weeks') ? periodQty * 7 : periodQty * 30;
+    return days;
+  };
+
+  function impactFn(params) {
+    const objImpact = {};
+    const impactDays = dayPeriodFn(params.periodType, params.timeToElapse);
+    objImpact.currentlyInfected = params.reportedCases * 10;
+    objImpact.infectionsByRequestedTime = infectionsByRequestedTimeFn(
+      objImpact.currentlyInfected, impactDays
+    );
+    return objImpact;
+  }
+
+  function severeImpactFn(params) {
+    const objSevereImpact = {};
+    const severeDays = dayPeriodFn(params.periodType, params.timeToElapse);
+    objSevereImpact.currentlyInfected = params.reportedCases * 50;
+    objSevereImpact.infectionsByRequestedTime = infectionsByRequestedTimeFn(
+      objSevereImpact.currentlyInfected,
+      severeDays
+    );
+
+    return objSevereImpact;
+  }
+  fatObj.data = data;
+
+  if (data) {
+    fatObj.impact = impactFn(data);
+    fatObj.severeImpact = severeImpactFn(data);
+  }
+
+  return fatObj;
+};
 
 export default covid19ImpactEstimator;
